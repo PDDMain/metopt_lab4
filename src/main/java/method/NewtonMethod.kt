1d51d3c93ebc188d0719ed1bd3bd6ca159a3118a
PDDMain <polzikdanden@gmain.com>
2021-06-30 17:00:34 +0300

    fix code

--- src/main/java/method/NewtonMethod.kt
+++ src/main/java/method/NewtonMethod.kt
@@ -4,20 +4,20 @@ import math.ScalarFunction
 import math.matrix.Vector
 
 abstract class NewtonMethod : MinimizeMethod {
+    val MAX_ITERATION = 100_000
+
     override fun minimize(
         function: ScalarFunction,
         startPoint: Vector,
         inaccuracy: Double
     ): Vector {
         var x = init(function, startPoint, inaccuracy)
-
-        while (true) {
+        var iter = 0
+        do {
             val delta = iterationStep(function, x, inaccuracy)
             x += delta
 
-            if (delta.norm() < inaccuracy)
-                break
-        }
+        } while (delta.norm() > inaccuracy && ++iter < MAX_ITERATION)
 
         return x
     }
